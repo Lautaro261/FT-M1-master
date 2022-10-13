d29ffff38f78cf0d90b153f7e7883162678b2345
Lautaro261 <nahuelygoofy@gmail.com>
2022-10-13 14:53:20 -0300

    Homework Realizada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,28 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  console.log(num)
-  
+  let arr = num.split("")
+  arr.reverse();
+  console.log(arr);
+ var acumulador = 0
+  for(let i = 0; i < arr.length; i++ ){
+    acumulador = acumulador + arr[i] * 2 ** i
+  }
+    return acumulador;
 }
 function DecimalABinario(num) {
   // tu codigo aca
+  var array = []
+  while (num !== 0) {
+     array.push(num % 2)
+     num = parseInt(num/2)
+   }
+ array = array.reverse()
+ var string = ""
+ for (let i=0; i<array.length ; i++){
+     string = string + array[i];
+    }
+  return string
 
 }
 
